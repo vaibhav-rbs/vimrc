b83ca8da10b070cf92d630a6d6da4ffefc303937
vaibhav-rbs <vaibhav-rbs@users.noreply.github.com>
2015-10-19 22:25:50 -0700

    added set nu, set paste , remember line

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -7,6 +7,12 @@ source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
 
+set nu
+set paste
+if has("autocmd")
+  au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
+endif
+
 try
 source ~/.vim_runtime/my_configs.vim
 catch
